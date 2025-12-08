ca372b538a9cf59251b70c708fe2a7e82051540e
rainndrn <andrianiraina27@gmail.com>
2025-12-08 20:10:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("Menggunakan **XGBoost Regression**")
 # ============================
 # LOAD DATA
 # ============================
-FILE_PATH = "data/cabai merah besar.csv"  # ubah kalau nama file berbeda
+FILE_PATH = "data/cabai_merah_besar.csv"  # ubah kalau nama file berbeda
 
 @st.cache_data
 def load_data():
@@ -105,4 +105,5 @@ st.success("Prediksi selesai!")
 
 st.write("📌 Model: XGBoost Regression")
 st.write("👩‍💻 Dibuat dengan Streamlit")
+
 
